d21d0279316ccc7e9641f4fa8291bb87c5311529
alex-bro <alexbronnikov76@gmail.com>
2016-08-16 19:00:07 +0300

    main_pic

--- wp-content/themes/stomat/page-golovna.php
+++ wp-content/themes/stomat/page-golovna.php
@@ -1,12 +1,16 @@
 <?php get_header() ?>
 		<div class="container">
 			<div class="top_content">
-				<div class="row">
-					<div class="logo">
-						<img src="<?php echo bloginfo('template_directory') ?>/images/logo-big.svg" alt="">
-					</div>
-					<div class="site-description">
-						<?php echo bloginfo('description') ?>
+				<div class="grad_left">
+					<div class="grad_right">
+						<div class="row">
+							<div class="logo">
+								<img src="<?php echo bloginfo('template_directory') ?>/images/logo-big.svg" alt="">
+							</div>
+							<div class="site-description">
+								<?php echo bloginfo('description') ?>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
