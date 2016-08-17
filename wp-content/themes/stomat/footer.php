2eadd24dcfb005e91517d60876dc019826065d95
alex-bro <alexbronnikov76@gmail.com>
2016-08-17 18:00:47 +0300

    severin

--- wp-content/themes/stomat/footer.php
+++ wp-content/themes/stomat/footer.php
@@ -1,12 +1,11 @@
-</div>
-<div class="row">
-	<div class="col-md-12">
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="name_site"><?php echo bloginfo('blogname') ?></div>
-			<div class="copy">2016 <?php echo __('all rights reserved', 'stomat') ?> ©</div>
-		</footer>
+	<div class="row footer_wrap">
+		<div class="col-md-12">
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="name_site"><?php echo bloginfo('blogname') ?></div>
+				<div class="copy">2016 <?php echo __('all rights reserved', 'stomat') ?> ©</div>
+			</footer>
+		</div>
 	</div>
-</div>
 </div>
 
 <?php echo '<script>'.AbvFunctions::get_text_by_slag('mapa').'</script>' ?>
